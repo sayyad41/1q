6aa0d2598ceeff7f0b3f8d6f489a9445f83b6d8c
sayyad41 <50000878+sayyad41@users.noreply.github.com>
2019-05-07 19:00:12 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,7 +90,7 @@ m.sendMessage(args)
    }
    });
 
-const developers = ["472413769700474901","575070857995747328"]
+const developers = ["472413769700474901","575478633259335690"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
